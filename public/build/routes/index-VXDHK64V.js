edbf833bbfe9304c521beba316ba6f066edda338
Junior Martins <amjr.box@gmail.com>
2022-02-10 12:05:28 -0300

    Untrack files in .gitignore

--- public/build/routes/index-VXDHK64V.js
+++
@@ -1,7 +0,0 @@
-import {
-  init_react
-} from "/build/_shared/chunk-E7VMOUYL.js";
-
-// browser-route-module:D:\webapps\remix-jokes\app\routes\index.tsx?browser
-init_react();
-//# sourceMappingURL=/build/routes/index-VXDHK64V.js.map
--- public/build/routes/jokes-6OIBQMPK.js
+++
@@ -1,7 +0,0 @@
-import {
-  init_react
-} from "/build/_shared/chunk-E7VMOUYL.js";
-
-// browser-route-module:D:\webapps\remix-jokes\app\routes\jokes.tsx?browser
-init_react();
-//# sourceMappingURL=/build/routes/jokes-6OIBQMPK.js.map
